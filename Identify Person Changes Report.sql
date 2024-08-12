1f61af2a457a08b1be9d51f7428c41e1cc4bfc59
mcdude8675309 <jamcclure@gmail.com>
2024-08-12 14:06:21 -0500

    Fixed casing and index note

--- Identify Person Changes Report.sql
+++ Identify Person Changes Report.sql
@@ -1,45 +1,43 @@
---Identify When non-staff changes Gender, BirthDate, Nickname, or FirstName 
---This is an opportunity for pastoral care, or to potentially catch mistakes that put people in the wrong groups
-select
-	h.EntityID as PersonID,
-	h.CreatedDateTime As ChangeDateTime,
-	h.Caption as Name,
-	h.ValueName As WhatChanged,
+/*
+	10/20/2022 - Jeff McClure
+	Identify When non-staff changes Gender, BirthDate, Nickname, or FirstName 
+	This is an opportunity for pastoral care, or to potentially catch mistakes that put people in the wrong groups
+    
+    Suporting Index:
+    Create NonClustered Index IX_History_CreatedDateTime_JAM_20221020 On dbo.History (CreatedDateTime, EntityTypeID) With (FillFactor=100, DATA_COMPRESSION=PAGE) On [Primary]
+*/
+SELECT
+	h.EntityId AS PersonID,
+	h.CreatedDateTime AS ChangeDateTime,
+	h.Caption AS Name,
+	h.ValueName AS WhatChanged,
 	h.OldValue,
 	h.NewValue,
-	Case When h.ValueName = 'Birth Date' Then Cast(Datediff(year, try_cast(h.OldValue as Date), Cast(Getdate() as Date)) as Varchar(10)) Else '-' End as OldAge,
-	Case When h.ValueName = 'Birth Date' Then Cast(Datediff(year, try_cast(h.NewValue as Date), Cast(Getdate() as Date)) as Varchar(10)) Else '-' End as NewAge,
-	Case p.Gender When 1 Then 'Male' When 2 Then 'Female' Else 'Unknown' End As CurrGender,
-	p.Birthdate As CurrBirthDate,
-	p.NickName As CurrNickName,
-	p.FirstName As CurrFirstName,
-	Concat (WhoDunnit.nickname, ' ', WhoDunnit.LastName) as WhoDoneIt
-From dbo.History h
-Inner Join dbo.Person p on p.Id = h.EntityId
-Inner Join dbo.PersonAlias pa on pa.id = h.CreatedByPersonAliasId
-Inner Join dbo.Person WhoDunnit on whodunnit.id = pa.PersonId
-Where 
-	h.CreatedDateTime >= Cast(Dateadd(Day, -8, Getdate()) As Datetime)
-	And h.CreatedDateTime < Cast(Cast(Getdate() as Date) As Datetime)
-	And Verb = 'MODIFY'
-	And EntityTypeId = 15 --Person
-	And ChangeType = 'Property'
-	And Valuename in ('Nick Name','First Name','Birth Date','Gender')
-	And OldValue Is Not Null
-	And OldValue <> 'Unknown'
-	And NewValue <> OldValue
-	And Not Exists (Select * from dbo.TaggedItem ti Where ti.TagID = 1 and ti.EntityGuid = WhoDunnit.Guid) --Not changed by Staff
-Order By Case ValueName
-		When 'Gender' Then 1
-		When 'Birth Date' Then 2
-		When 'Nick Name' Then 3
-		Else 4 End,
+	CASE WHEN h.ValueName = 'Birth Date' THEN CAST(DATEDIFF(YEAR, TRY_CAST(h.OldValue AS DATE), CAST(GETDATE() AS DATE)) AS VARCHAR(10)) ELSE '-' END AS OldAge,
+	CASE WHEN h.ValueName = 'Birth Date' THEN CAST(DATEDIFF(YEAR, TRY_CAST(h.NewValue AS DATE), CAST(GETDATE() AS DATE)) AS VARCHAR(10)) ELSE '-' END AS NewAge,
+	CASE p.Gender WHEN 1 THEN 'Male' WHEN 2 THEN 'Female' ELSE 'Unknown' END AS CurrGender,
+	p.BirthDate AS CurrBirthDate,
+	p.NickName AS CurrNickName,
+	p.FirstName AS CurrFirstName,
+	CONCAT (WhoDunnit.NickName, ' ', WhoDunnit.LastName) AS WhoDoneIt
+FROM dbo.History h
+INNER JOIN dbo.Person p ON p.Id = h.EntityId
+INNER JOIN dbo.PersonAlias pa ON pa.Id = h.CreatedByPersonAliasId
+INNER JOIN dbo.Person WhoDunnit ON WhoDunnit.Id = pa.PersonId
+WHERE 
+	h.CreatedDateTime >= CAST(DATEADD(DAY, -8, GETDATE()) AS DATETIME)
+	AND h.CreatedDateTime < CAST(CAST(GETDATE() AS DATE) AS DATETIME)
+	AND Verb = 'MODIFY'
+	AND EntityTypeId = 15 --Person
+	AND ChangeType = 'Property'
+	AND ValueName IN ('Nick Name','First Name','Birth Date','Gender')
+	AND OldValue IS NOT NULL
+	AND OldValue <> 'Unknown'
+	AND NewValue <> OldValue
+	AND NOT EXISTS (SELECT * FROM dbo.TaggedItem ti WHERE ti.TagId = 1 AND ti.EntityGuid = WhoDunnit.Guid) --Not changed by Staff
+ORDER BY CASE ValueName
+		WHEN 'Gender' THEN 1
+		WHEN 'Birth Date' THEN 2
+		WHEN 'Nick Name' THEN 3
+		ELSE 4 END,
 		h.CreatedDateTime
-		
---Create NonClustered Index IX_History_CreatedDateTime_JAM_20221020 On dbo.History (CreatedDateTime, EntityTypeID) With (FillFactor=100) On [Primary]
-
---select top 100 * from tag
---select top 100 * from TaggedItem
-
-
---Select * from attribute where id = 107658 or name = 'coach'
